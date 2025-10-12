48ba3c0a0642830b093b85067edbe585c3851d92
Michael Rahne <mrahne4@gmail.com>
2025-10-12 11:48:31 -0400

    Update firewall_traffic_analyzer.py

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -31,7 +31,7 @@ def fire_traf():
 
     if port_number == 22 or 3389 and transfer_size > 500:
         print('Risk Assessment: HIGH RISK: Potential unauthorized remote access detected!')
-    elif port_number == 80 or 3389 and transfer_size >= 100:
+    elif port_number == 22 or 3389 and transfer_size > 100:
         print('Risk Assessment: MEDIUM RISK: Large unencrypted data transfer detected.')
     elif port_number == 80 and transfer_size > 100:
         print('Risk Assessment: MEDIUM RISK: Large unencrypted data transfer detected.')
